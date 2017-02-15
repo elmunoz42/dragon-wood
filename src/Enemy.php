db31291e2da2ca6530fa6708e04ed42f8cc1d94e
Carlos Munoz Kampff and David Quisenberry <david@>
2017-02-15 10:00:07 -0800

    pass EnemyTest successfully.

--- src/Enemy.php
+++ src/Enemy.php
@@ -1,4 +1,44 @@
-<?php  class Enemy {
+<?php
 
-}
+    class Enemy {
+
+        private $name;
+        private $value;
+        private $strike_req;
+        private $stomp_req;
+        private $shout_req;
+
+
+        function __construct($i_name, $i_value, $i_strike_req, $i_stomp_req, $i_shout_req) {
+            $this->name = $i_name;
+            $this->value = $i_value;
+            $this->strike_req = $i_strike_req;
+            $this->stomp_req = $i_stomp_req;
+            $this->shout_req = $i_shout_req;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+        function getValue()
+        {
+            return $this->value;
+        }
+        function getStrikeReq()
+        {
+            return $this->strike_req;
+        }
+        function getStompReq()
+        {
+            return $this->stomp_req;
+        }
+        function getShoutReq()
+        {
+            return $this->shout_req;
+        }
+
+
+    }
+
 ?>
--- tests/EnemyTest.php
+++ tests/EnemyTest.php
@@ -5,7 +5,21 @@ class EnemyTest extends PHPUnit_Framework_TestCase
 
     function test_checkEnemy1()
     {
-
+        // Arrange
+        $i_name = "Dragon";
+        $i_value = 3;
+        $i_strike_req = 4;
+        $i_stomp_req = 5;
+        $i_shout_req = 6;
+        $testEnemy = new Enemy($i_name, $i_value, $i_strike_req, $i_stomp_req, $i_shout_req);
+        // Act
+        $result = $testEnemy;
+        // Assert
+        $this->assertEquals("Dragon", $result->getName());
+        $this->assertEquals(3, $result->getValue());
+        $this->assertEquals(4, $result->getStrikeReq());
+        $this->assertEquals(5, $result->getStompReq());
+        $this->assertEquals(6, $result->getShoutReq());
     }
   }
   ?>
