f3e6037d7f9a01edba477ca162668bebed2ba818
Carlos Munoz Kampff and David Quisenberry <david@>
2017-02-15 12:49:34 -0800

    pass test-checkGameManager.

--- src/GameManager.php
+++ src/GameManager.php
@@ -1,4 +1,32 @@
-<?php  class GameManager {
+<?php
+    class GameManager
+    {
+        // array of 5 opponent cards which are eligible for attack.
+        private $landscape = array();
+        // player one Player object.
+        private $player_one;
+        // player two Player object.
+        private $player_two;
+        // deck of cards for player attacks to be drawn from.
+        private $deck_of_cards = array();
+        // cards to populate the landscape.
+        private $deck_of_opponents = array();
+        // cards that players have played.
+        private $played_cards = array();
 
-}
+        function __construct()
+        {
+
+        }
+
+        function setPlayerOne($player_object)
+        {
+            $this->player_one = $player_object;
+        }
+        function getPlayerOne()
+        {
+            return $this->player_one;
+        }
+    }
+
 ?>
--- tests/GameManagerTest.php
+++ tests/GameManagerTest.php
@@ -3,9 +3,22 @@ require_once "src/GameManager.php";
 class GameManegerTest extends PHPUnit_Framework_TestCase
 {
 
-    function test_checkGameManeger1()
+    function test_checkGameManager()
     {
+        // Arrange
+        $test_game = new GameManager();
+        $i_player_name ="Steve";
+        $i_hand = array(new Card(1,"blue"));
+        $i_attack_cards = array(new Card(2,"red"));
+        $i_attack_type = "strike";
+        $i_captured_cards = array(new Enemy("Goblin",1,2,3,4));
+        $test_player = new Player($i_player_name, $i_hand, $i_attack_cards, $i_attack_type, $i_captured_cards);
+        $test_game->setPlayerOne($test_player);
 
+        // Act
+        $result = $test_game;
+        // Assert
+        $this->assertEquals("Steve", $test_game->getPlayerOne()->getName());
     }
 
   }
