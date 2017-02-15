35d533d51068737a9da71d95a7dcc6deb19a928b
Carlos Munoz Kampff and David Quisenberry <carlos@>
2017-02-15 14:21:44 -0800

    pass test_tallyScore.

--- src/GameManager.php
+++ src/GameManager.php
@@ -13,6 +13,13 @@
         private $deck_of_opponents = array();
         // cards that players have played.
         private $played_cards = array();
+        // diceRoll.
+        private $dice_roll;
+        // playerOneScore
+        private $player_one_score = 0;
+        // playerTwoScore
+        private $player_two_score = 0;
+
 
         function __construct()
         {
@@ -37,10 +44,48 @@
             $card_position = array_search ( $card , $this->deck_of_opponents );
             array_splice ( $this->deck_of_opponents , $card_position, 1 );
         }
-
-
+        function diceRoll($size)
+        {
+            $roll = rand(1, $size);
+            $this->dice_roll = $roll;
+        }
+        function getDiceRoll()
+        {
+            return $this->dice_roll;
+        }
+        function tallyScore()
+        {
+            $p_one_captured = $this->player_one->getCapturedCards();
+            $p_one_sum = 0;
+            foreach ($p_one_captured as $card) {
+                $p_one_sum+= $card->getValue();
+            }
+            $this->setPlayerOneScore($p_one_sum);
+            $p_two_captured = $this->player_two->getCapturedCards();
+            $p_two_sum = 0;
+            foreach ($p_two_captured as $card) {
+                $p_two_sum+= $card->getValue();
+            }
+            $this->setPlayerTwoScore($p_two_sum);
+        }
 
         // Setters and Getters
+        function setPlayerOneScore($score)
+        {
+            $this->player_one_score = $score;
+        }
+        function getPlayerOneScore()
+        {
+            return $this->player_one_score;
+        }
+        function setPlayerTwoScore($score)
+        {
+            $this->player_two_score = $score;
+        }
+        function getPlayerTwoScore()
+        {
+            return $this->player_two_score;
+        }
         function setPlayerOne($player_object)
         {
             $this->player_one = $player_object;
